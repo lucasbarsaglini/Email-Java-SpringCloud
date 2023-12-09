35c42dfc343848d4fa545912734ccb9a9b03bf68
lucasbarsaglini <lucasbarsag@gmail.com>
2023-12-08 21:40:20 -0300

    User test: service e producer

--- user/src/main/java/producers/UserProducer.java
+++ user/src/main/java/producers/UserProducer.java
@@ -1,5 +1,6 @@
 package producers;
 
+import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -9,24 +10,34 @@ import models.UserModel;
 
 @Component
 public class UserProducer {
-	
-	final RabbitTemplate rabbitTemplate;
-	
-	public UserProducer(RabbitTemplate rabbitTemplate) {
-		this.rabbitTemplate = rabbitTemplate;
-	}
-	
-	@Value(value = "${broker.queue.email.name}")
-	private String routingKey;
-	
-	public void publishMessageEmail(UserModel userModel) {
-		var emailDto = new EmailDto();
-		emailDto.setUserId(userModel.getUserId());
-		emailDto.setEmailTo(userModel.getEmail());
-		emailDto.setSubject("Cadastro realizado com sucesso!");
-		emailDto.setText(userModel.getName() + ", seja bem vindo()!");
-		
-		rabbitTemplate.convertAndSend("", routingKey, emailDto);
-	}
 
+    private final RabbitTemplate rabbitTemplate;
+
+    @Value(value = "${broker.queue.email.name:default_routing_key}")
+    private String routingKey;
+
+    public UserProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void publishMessageEmail(UserModel userModel) {
+        if (userModel == null) {
+            throw new IllegalArgumentException("O usuário não pode ser null");
+        }
+
+        if (userModel.getUserId() == null || userModel.getEmail() == null || userModel.getName() == null) {
+            throw new IllegalArgumentException("O usuário deve conter id, email e name");
+        }
+
+        var emailDto = new EmailDto();
+        emailDto.setUserId(userModel.getUserId());
+        emailDto.setSubject("Cadastro realizado com sucesso!");
+        emailDto.setText(userModel.getName() + ", seja bem vindo()!");
+
+        try {
+            rabbitTemplate.convertAndSend("", routingKey, emailDto);
+        } catch (AmqpException ex) {
+			System.err.println(ex);
+		}
+    }
 }
